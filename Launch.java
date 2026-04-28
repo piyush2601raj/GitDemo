2d59cdfce5c4b8cd17a199a6dc2d07289b44c620
Piyush Mishra <piyush26@gmail.com>
2026-04-28 16:43:03 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -2,4 +2,9 @@ class Launch{
 public static void main(String[] args){
                  System,out.println("Exploring GIT & GitHub!");
 }
+public void fun 1(){
+int a =12;
+int b=23;
+int c=a+b;
+System.out.println(c):
 }
